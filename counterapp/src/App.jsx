9ba3f917558a32e1b2c827b87a80365bc50c001a
ajay <bingishettyajay@gmail.com>
2025-10-23 13:38:18 +0530

    counter app project-2 of level-1

---
+++ counterapp/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+function App() {
+ const [counter, setCount] = useState(1)
+ let bouncehandled = ()=>{
+   if (counter > 0 && counter < 20){
+    setCount(counter =>counter + 1)
+  }
+ }
+
+ let struggled = ()=>{
+  if (counter > 0 && counter <= 20){
+    setCount(counter =>counter - 1);
+  }
+ }
+//  Effective range: 0 to 20 (21 possible values)
+
+  return (
+    <>
+      <h1>Australian Bouncy Pitches</h1>
+      <h2>Head to Head {counter}</h2>
+      <button onClick={bouncehandled}>play {counter}</button>
+       <br />
+       <button onClick={struggled}>well left {counter}</button>
+    </>
+  )
+}
+
+export default App
+//  bridge between your HTML page and React application
---
+++ counterapp/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+/* Detects unsafe lifecycles - warns about deprecated methods
+Warns about legacy APIs - like string refs, findDOMNode
+Double-invokes functions - to detect side effects:
+
+Component functions render twice
+useState initializers run twice*/
